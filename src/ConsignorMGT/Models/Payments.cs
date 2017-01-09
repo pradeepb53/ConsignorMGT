0b5648a182a61164a8fa398aade8516164c18762
Pradeep Biyagamage <pradeepb@shaw.ca>
2017-01-09 14:52:59 -0800

    Add a new model class-Payments, changed home view

---
+++ src/ConsignorMGT/Models/Payments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsignorMGT.Models
+{
+    public class Payments
+    {
+        public int PaymentID { get; set; }
+        public string OwnerCode { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        public int Amount { get; set; }
+
+        public string Payable { get; set; }
+    }
+}
